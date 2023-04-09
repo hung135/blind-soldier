02b00ba8895f340e3265bc95cfc8149b30444178
hung135 <hung135@hotmail.com>
2023-04-09 18:16:35 -0400

    Update soldier.py

--- soldier.py
+++ soldier.py
@@ -1,9 +1,7 @@
 import socket
 import struct
 import threading
-import time
 import json
-import argparse
 
 MULTICAST_GROUP = '224.0.0.1'
 PORT = 12345
@@ -13,32 +11,33 @@ class Soldier:
         self.ip = ip
         self.name = name
         self.task_list = task_list
-        self.peers = []
+        self.peers = {}
         self.task_assignment = {self.name: task_list}
 
     def new_peer(self, name, ip):
-        self.peers.append((name, ip))
+        self.peers[name] = ip
         self.divide_tasks()
 
     def remove_peer(self, name):
-        self.peers = [peer for peer in self.peers if peer[0] != name]
-        self.divide_tasks()
+        if name in self.peers:
+            del self.peers[name]
+            self.divide_tasks()
 
     def divide_tasks(self):
-        all_tasks = self.task_list.copy()
-        for _, tasks in self.task_assignment.items():
-            all_tasks.extend(tasks)
-
+        all_tasks = [task for tasks in self.task_assignment.values() for task in tasks]
         num_peers = len(self.peers) + 1
-        num_tasks_per_peer = len(all_tasks) // num_peers
+        tasks_per_peer, remainder = divmod(len(all_tasks), num_peers)
 
-        self.task_assignment = {self.name: all_tasks[:num_tasks_per_peer]}
-        for i, (name, _) in enumerate(self.peers):
-            start = (i + 1) * num_tasks_per_peer
-            end = (i + 2) * num_tasks_per_peer
-            self.task_assignment[name] = all_tasks[start:end]
+        task_assignments = {}
+        task_index = 0
 
-        print(f"{self.name}'s Task Assignments: {self.task_assignment}")
+        for peer_name in [self.name, *self.peers.keys()]:
+            num_tasks = tasks_per_peer + (1 if remainder > 0 else 0)
+            task_assignments[peer_name] = all_tasks[task_index : task_index + num_tasks]
+            task_index += num_tasks
+            remainder -= 1
+
+        self.task_assignment = task_assignments
 
     def send_message(self, message):
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
@@ -65,25 +64,3 @@ class Soldier:
 
     def listen(self):
         threading.Thread(target=self.receive_message).start()
-
-def main(ip, name):
-    tasks = [f"Task{str(i).zfill(2)}" for i in range(1, 21)]
-
-    soldier = Soldier(ip, name, tasks)
-    soldier.listen()
-
-    time.sleep(1)
-
-    soldier.send_message({"action": "new_peer", "name": soldier.name, "ip": soldier.ip})
-
-    time.sleep(5)
-
-    soldier.send_message({"action": "remove_peer", "name": soldier.name})
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Start a soldier process.")
-    parser.add_argument("ip", type=str, help="IP address of the soldier")
-    parser.add_argument("name", type=str, help="Name of the soldier")
-
-    args = parser.parse_args()
-    main(args
